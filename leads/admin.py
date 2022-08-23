1024a2a42e43a45e48d438a9ff4433789229af07
bruno.miglioretto <bruno.miglioretto@obraprimaweb.com.br>
2022-08-22 23:55:50 -0300

    add admin pages

--- leads/admin.py
+++ leads/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from leads.models import Agent, Lead
 
-# Register your models here.
+User = get_user_model()
+
+admin.site.register(Agent)
+admin.site.register(Lead)
+admin.site.register(User)
