20185366edb138631bd4a62b2dd2e4b4758ac380
bruno.miglioretto <bruno.miglioretto@obraprimaweb.com.br>
2022-08-24 23:54:48 -0300

    add context to template page

--- leads/views.py
+++ leads/views.py
@@ -1,5 +1,13 @@
+from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Lead
+
 
 def home_page(request):
-    return render(request, "second_page.html")
+    leads = Lead.objects.all()
+    context = {
+        "leads": leads
+    }
+    return render(request, "second_page.html", context)
+
